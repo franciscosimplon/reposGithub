817fa7a953016fc7a71008a9b466050e533ea41f
Francisco <fco.fit@gmail.com>
2022-05-02 16:08:47 +0200

    third (user)

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
 
-// let pagina = 1;
-// const btnAnterior = document.getElementById('btnAnterior')
-// const btnSiguiente = document.getElementById('btnSiguiente')
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior')
+const btnSiguiente = document.getElementById('btnSiguiente')
 
-// btnSiguiente.addEventListener('click', () => {
-// 	if (pagina < 1000) {
-// 		pagina += 1;
-// 		cargarRepos();
-// 	}
-// });
-// btnAnterior.addEventListener('click', () => {
-// 	if (pagina > 1) {
-// 		pagina -= 1;
-// 		cargarRepos();
-// 	}
-// });
-// const form = document.querySelector('#form');
-// const bar = document.querySelector('#search');
+btnSiguiente.addEventListener('click', () => {
+	if (pagina < 1000) {
+		pagina += 1;
+		cargarRepos();
+	}
+});
+btnAnterior.addEventListener('click', () => {
+	if (pagina > 1) {
+		pagina -= 1;
+		cargarRepos();
+	}
+});
+const form = document.querySelector('#form');
+const bar = document.querySelector('#search');
 
-// window.addEventListener('load', () => {
-// 	form.addEventListener('submit', buscarRepos);
-// })
-
-
-// function buscarRepos(e) {
-// 	e.preventDefault();
-
-// 	// const barValue = document.querySelector('#search').value;
-
-// 	cargarRepos(bar)
-// }
+window.addEventListener('load', () => {
+	form.addEventListener('submit', buscarRepos);
+})
 
 
-// const cargarRepos = async () => {
+function buscarRepos(e) {
+	e.preventDefault();
+
+	// const barValue = document.querySelector('#search').value;
+
+	cargarRepos(bar)
+}
+
+
+const cargarRepos = async () => {
 	
-// 	try {
-// 		const respuesta = await fetch(`https://api.github.com/users/${bar.value}/repos?page=${pagina}&per_page=4`);
+	try {
+		const respuesta = await fetch(`https://api.github.com/users/${bar.value}/repos?page=${pagina}&per_page=4`);
 
-// 		const datos = await respuesta.json();
-// 		console.log(datos);
-// 		let repos = ``;
+		const datos = await respuesta.json();
+		console.log(datos);
+		let repos = ``;
 
-// 		datos.forEach(repo => {
-// 			repos += `
-// 			<div class="repo">
-// 			<h3 class="inside"> <strong> Id: </strong> ${repo.id}</h3>
-// 			<h3 class="inside"> <strong> Name: </strong> ${repo.name}</h3>
-// 			<p class="inside"> <strong> Description: </strong> ${repo.description}</p>
-// 			<p class="inside"> <strong> Language:  </strong> ${repo.language}</p>
-// 			<p class="inside"> <strong> Pushed_At: </strong> ${repo.pushed_at}</p>
-// 			<p class="inside"> <strong> Created_At: </strong> ${repo.created_at}</p>
-// 			<p class="inside"> <strong> Updated_At: </strong> ${repo.update_at}</p>
-// 			<a href="${repo.url}" class="inside"> Repo info </a>
-// 			// <a href="${repo.events_url} " class="inside">Events repo </a>
-// 			// <a href="${repo.commits_url}" class="inside">Commits info,  </a>
-// 			// <a href="${repo.pulls_url}" class="inside"> Pulls info </a>
-// 			<a href="${repo.merges_url} " class="inside">Merges info  </a>
-// 			</div>
-// 			`;
+		datos.forEach(repo => {
+			repos += `
+			<div class="repo">
+			<h3 class="inside"> <strong> Id: </strong> ${repo.id}</h3>
+			<h3 class="inside"> <strong> Name: </strong> ${repo.name}</h3>
+			<p class="inside"> <strong> Description: </strong> ${repo.description}</p>
+			<p class="inside"> <strong> Language:  </strong> ${repo.language}</p>
+			<p class="inside"> <strong> Pushed_At: </strong> ${repo.pushed_at}</p>
+			<p class="inside"> <strong> Created_At: </strong> ${repo.created_at}</p>
+			<p class="inside"> <strong> Updated_At: </strong> ${repo.update_at}</p>
+			<a href="${repo.url}" class="inside"> Repo info </a>
+			// <a href="${repo.events_url} " class="inside">Events repo </a>
+			// <a href="${repo.commits_url}" class="inside">Commits info,  </a>
+			// <a href="${repo.pulls_url}" class="inside"> Pulls info </a>
+			<a href="${repo.merges_url} " class="inside">Merges info  </a>
+			</div>
+			`;
 
 			
-// 		});
+		});
 
-// 		document.getElementById('contenedor').innerHTML = repos;
+		document.getElementById('contenedor').innerHTML = repos;
 
 
-// 	}catch(error){
-// 		console.log(error);
-// 	}
-// }
+	}catch(error){
+		console.log(error);
+	}
+}
 
-// cargarRepos();
+cargarRepos();
